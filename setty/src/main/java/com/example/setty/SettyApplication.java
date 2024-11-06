274e86d7e0d8c3b26e646cea3b54bbc9f1e6aa76
xxubin04 <yubinjeong40@gmail.com>
2024-11-06 15:34:14 +0900

    Add: initial setting and config

---
+++ setty/src/main/java/com/example/setty/SettyApplication.java
@@ -0,0 +1,13 @@
+package com.example.setty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SettyApplication.class, args);
+    }
+
+}
