af802d1cac702a449d48270bea4698eab5c5e13f
xxubin04 <yubinjeong40@gmail.com>
2025-01-03 13:32:46 +0900

    에러 수정

---
+++ src/main/java/com/example/setty/common/auth/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.setty.common.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // 모든 엔드포인트에 대해 CORS 허용
+                .allowedOrigins("http://172.18.157.29:80")  // 모든 엔드포인트에 대해 CORS 허용
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 허용할 HTTP 메서드
+                .allowedHeaders("*")  // 모든 헤더 허용
+                .allowCredentials(true);  // 인증 정보 허용
+    }
+}
